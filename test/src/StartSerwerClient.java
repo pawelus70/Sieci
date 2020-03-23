2befe2e701ab07f952b85d83220a834e11c635e0
pawelus70 <pblak1998@gmail.com>
2020-03-23 16:04:31 +0100

    Pierwsza wersja komunikatora

---
+++ test/src/StartSerwerClient.java
@@ -0,0 +1,13 @@
+import TestClient.TestClient;
+import TestServer.TestServer;
+
+public class StartSerwerClient {
+    public static void main(String[] argv){
+        TestServer serwer = new TestServer();
+        serwer.serverSetup();
+
+        TestClient klient = new TestClient();
+        klient.createWorksheet();
+
+    }
+}
