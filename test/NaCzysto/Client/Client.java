a23fccc93178f0944cc46ec28e24d182a1b30739
Astaree <cwiekg@gmail.com>
2020-04-25 21:59:43 +0200

    Klient:    -klient jest zmuszony do dodania imienia - brak imienia domyślnego, regex litery-liczby bez symboli    -dodano polecenia tekstowe:           -000 - echo (np.: "000")           -001 - pobierz listę osób obecnych na serwerze (np.: "001")           -002 - zmień imię (np.: "002TwojeImie" -> "TwojeImie", "002 TwojeImie" -> " TwojeImie")           -003 - wyślij do wszystkich (np.: "003Wiadomość" -> "TwojeImie: Wiadomość", "003 Wiadomość" -> " TwojeImie:  Wiadomość")    -dodano wyświetlanie połączonych klientów    -obsługa echa
    Serwer:
       -obsługa poleceń tekstowych (ww.)
       -obliczanie pingu
       -inne drobne zmiany
    
    TODO:
       -Whispers (trudność 2/10)
       -Komenda rozłącz (999)
       -Rozmowy grupowe (trudność 5/7 - pomysł jest, bez próby)

--- test/NaCzysto/Client/Client.java
+++ test/NaCzysto/Client/Client.java
@@ -1,72 +1,145 @@
-import java.net.*;
 import java.io.*;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
+import java.net.*;
+import java.util.ArrayList;
+import java.util.concurrent.*;
+import java.util.regex.Pattern;
 
 
 public class Client {
+    String ip = "127.0.0.1";
+    int port = 4242;
+    Pattern pattern =Pattern.compile("^[a-zA-Z0-9]+$");
+    ArrayList<ClientList> connectedList = new ArrayList<>();
     private final ExecutorService threadPool = Executors.newFixedThreadPool(4);
 
-    public class ClientData implements Serializable {
+
+    public class ClientList {
+        String name;
+        int index;
+
+        ClientList(String name, int index) {
+            this.name = name;
+            this.index = index;
+        }
+    }
+
+
+    public class ClientHandle {
         String name;
         Socket socket;
         BufferedReader bufferedReader;
         PrintWriter printWriter;
         Boolean isConnected;
         InputStreamReader inputStreamReader;
+        int index;
 
         public void showStatus() {
             System.out.println("Is Connected: " + this.isConnected);
             System.out.println("Connected as: " + this.name);
+            System.out.println("Your index is: " + this.index);
+            System.out.println("To get list of commands type: \"help\"");
         }
     }
 
-    ClientData clientData = new ClientData();
+    ClientHandle clientHandle = new ClientHandle();
+
 
     public void connectToServer(String ip, int port) {
         try {
-            clientData.socket = new Socket(ip, port);
-            clientData.isConnected = true;
-            clientData.inputStreamReader = new InputStreamReader(clientData.socket.getInputStream());
-            clientData.bufferedReader = new BufferedReader(clientData.inputStreamReader);
-            clientData.printWriter = new PrintWriter(clientData.socket.getOutputStream());
+            clientHandle.socket = new Socket(ip, port);
+            clientHandle.isConnected = true;
+            //text
+            clientHandle.inputStreamReader = new InputStreamReader(clientHandle.socket.getInputStream());
+            clientHandle.bufferedReader = new BufferedReader(clientHandle.inputStreamReader);
+            clientHandle.printWriter = new PrintWriter(clientHandle.socket.getOutputStream());
+
+
+            //set name
+            whatIsYourName();
         } catch (Exception e) {
-            clientData.isConnected = false;
+            System.out.println("Can't connect with server.");
+            clientHandle.socket = null;
+            clientHandle.isConnected = false;
+            clientHandle.printWriter = null;
+            clientHandle.bufferedReader = null;
+            clientHandle.inputStreamReader = null;
+        }
+    }
+
+    public void whatIsYourName() {
+        clientHandle.printWriter.println(clientHandle.name);
+        clientHandle.printWriter.flush();
+
+        try {
+            clientHandle.index = Integer.decode(clientHandle.bufferedReader.readLine());
+        } catch (IOException e) {
             e.printStackTrace();
-            clientData.printWriter = null;
-            clientData.bufferedReader = null;
-            clientData.inputStreamReader = null;
         }
     }
 
     public void run() {
+
         try {
+            String name;
             System.out.println("Enter your name: ");
-            clientData.name = new BufferedReader(new InputStreamReader(System.in)).readLine();
-            if (clientData.name.equals("")) clientData.name = "Anonymous";
-            connectToServer("127.0.0.1", 4242);
-            clientData.showStatus();
-            threadPool.execute(new SendToEveryone());
+            while(true) {
+                name = new BufferedReader(new InputStreamReader(System.in)).readLine();
+                if(pattern.matcher(name).matches()){
+                    if(!(name.length()<3))break;
+                }
+                System.out.println("Name can only contain letters [A-Z], numbers [0-9] and its size must be larger than 3 characters.");
+            }
+            clientHandle.name=name;
+            connectToServer(ip, port);
+            clientHandle.showStatus();
+            threadPool.execute(new RequestListener());
             threadPool.execute(new WarningReceiver());
         } catch (Exception ex) {
             ex.printStackTrace();
         }
     }
 
-    public class SendToEveryone implements Runnable {
+    public class RequestListener implements Runnable {
         String message;
 
         public void run() {
-            while (clientData.isConnected) {
+            while (clientHandle.isConnected) {
                 try {
                     message = new BufferedReader(new InputStreamReader(System.in)).readLine();
                 } catch (IOException e) {
                     e.printStackTrace();
                 }
                 if (!(message.equals(""))) {
-                    clientData.printWriter.println(clientData.name + ": " + message);
-                    clientData.printWriter.flush();
-                    message = "";
+                    if (message.startsWith("000")) { //echo
+                        message += System.nanoTime();
+                        clientHandle.printWriter.println(message);
+                        clientHandle.printWriter.flush();
+                    } else if (message.startsWith("001")) { //client list
+                        clientHandle.printWriter.println("001");
+                        clientHandle.printWriter.flush();
+                    } else if (message.startsWith("002")) {//change name
+                        if(message.length()>6){
+                            if(pattern.matcher(message).matches()){
+                                clientHandle.printWriter.println(message);
+                                clientHandle.printWriter.flush();
+                                clientHandle.name = message.substring(3);
+                            }else{
+                                System.out.println("Name can only contain letters [A-Z], numbers [0-9] and its size must be larger than 3 characters.");
+                            }
+                        }else {
+                            System.out.println("Name cant be shorter than 3 signs");
+                        }
+                        clientHandle.printWriter.println(message);
+                        clientHandle.printWriter.flush();
+                    } else if (message.equals("003")) {//send to everyone
+                        clientHandle.printWriter.println(message);
+                        clientHandle.printWriter.flush();
+                    } else if(message.equals("004")) {//whisper
+
+                    }else{
+                        clientHandle.printWriter.println(message);
+                        clientHandle.printWriter.flush();
+                    }
                 }
             }
         }
@@ -77,13 +150,34 @@ public class Client {
 
         public void run() {
             try {
-                while (clientData.bufferedReader != null) {
-                    message = clientData.bufferedReader.readLine();
-                    System.out.println(message);
+                while (clientHandle.bufferedReader != null) {
+                    message = clientHandle.bufferedReader.readLine();
+                    if (message.startsWith("000")) {
+                        message = message.substring(3);
+                        long time = Long.parseLong(message) / 1000000;
+
+                        System.out.println("Ping: " + time + " ms");
+                    } else if (message.startsWith("001")) {
+                        connectedList.clear();
+                        message = message.substring(3);
+                        for (int i = 0; i < Integer.decode(message); i++) {
+                            connectedList.add(new ClientList(clientHandle.bufferedReader.readLine(), Integer.parseInt(clientHandle.bufferedReader.readLine())));
+                        }
+                        showConnected();
+
+                    } else {
+                        System.out.println(message);
+                    }
                 }
             } catch (IOException ex) {
                 ex.printStackTrace();
             }
+        }
+    }
+
+    public void showConnected() {
+        for (ClientList clientList : connectedList) {
+            System.out.println(clientList.name + " #" + clientList.index);
         }
     }
 
--- test/NaCzysto/Server/Server.java
+++ test/NaCzysto/Server/Server.java
@@ -1,28 +1,51 @@
-import java.net.*;
+import java.awt.*;
 import java.io.*;
+import java.net.*;
 import java.time.*;
-import java.util.*;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
+import java.util.ArrayList;
+import java.util.concurrent.*;
 
 
 public class Server {
-    ArrayList<PrintWriter> inputStreams;
-    private final ExecutorService threadPool = Executors.newCachedThreadPool();
-//    private final ExecutorService threadPool = Executors.newFixedThreadPool(200);
+    ArrayList<ClientHandle> clientHandles;
+    int amountConnected = 0;
+    // private final ExecutorService threadPool = Executors.newCachedThreadPool();
+    // private final ExecutorService threadPool = Executors.newFixedThreadPool(200);
+    private final ExecutorService threadPool = Executors.newWorkStealingPool();
+
+    public void ShareClientList(int index) {
+        clientHandles.get(index).printMessageWriter.println("001" + clientHandles.size());
+        clientHandles.get(index).printMessageWriter.flush();
+        for (int i = 0; i < clientHandles.size(); i++) {
+            clientHandles.get(index).printMessageWriter.println(clientHandles.get(i).name);
+            clientHandles.get(index).printMessageWriter.flush();
+            clientHandles.get(index).printMessageWriter.println(clientHandles.get(i).index);
+            clientHandles.get(index).printMessageWriter.flush();
+        }
+    }
 
     public class ClientHandle implements Runnable {
-        Socket socket;
-        BufferedReader bufferedReader;
-        InputStreamReader inputStreamReader;
+        String name;
+        int index;
+        Long time;
+        Socket socketMessage;
+        BufferedReader bufferedMessageReader;
+        InputStreamReader inputMessageStreamReader;
         Boolean isConnected = false;
+        PrintWriter printMessageWriter;
 
-        public ClientHandle(Socket clientSocket) {
+
+        public ClientHandle(Socket clientSocket, int index) {
             try {
-                socket = clientSocket;
-                inputStreamReader = new InputStreamReader((socket.getInputStream()));
-                bufferedReader = new BufferedReader(inputStreamReader);
+                socketMessage = clientSocket;
+                printMessageWriter = new PrintWriter(socketMessage.getOutputStream());
+                inputMessageStreamReader = new InputStreamReader((socketMessage.getInputStream()));
+                bufferedMessageReader = new BufferedReader(inputMessageStreamReader);
                 isConnected = true;
+                this.index = index;
+                printMessageWriter.println(index);
+                printMessageWriter.flush();
+
             } catch (Exception e) {
                 e.printStackTrace();
             }
@@ -31,45 +54,91 @@ public class Server {
         public void run() {
             String message;
             try {
-                while ((message = bufferedReader.readLine()) != null) {
-                    System.out.println(message);
-                    sendToEveryone(message);
+                while ((message = bufferedMessageReader.readLine()) != null) {
+                    time = System.nanoTime();
+                    System.out.println(name + ", " + LocalTime.now().withNano(0) + ", " + message);
+                    requestListener(message, index);
                 }
             } catch (IOException e) {
+                this.isConnected = false;
                 e.printStackTrace();
+                onDisconnection(index);
             }
         }
+
     }
 
-    public void sendToEveryone(String message) {
-        Iterator iterator = inputStreams.iterator();
-        try {
-            while (iterator.hasNext()) {
-                PrintWriter printWriter = (PrintWriter) iterator.next();
-                printWriter.println(message);
-                printWriter.flush();
+
+    public void requestListener(String message, int index) {
+        if (message.startsWith("000")) { //Echo
+
+            String TempTime = message.substring(3);
+            clientHandles.get(index).printMessageWriter.println("000" + (System.nanoTime() - Long.parseLong(TempTime)));
+            clientHandles.get(index).printMessageWriter.flush();
+
+
+        } else if (message.startsWith("001")) {//give list of users
+            ShareClientList(index);
+        } else if (message.startsWith("002")) {//change name
+            clientHandles.get(index).name = message.substring(3);
+        } else if (message.startsWith("003")) {//send to everyone
+            for (int i = 0; i < clientHandles.size(); i++) {
+                if (i == index) continue;
+                message = message.substring(3);
+                clientHandles.get(i).printMessageWriter.println(clientHandles.get(index).name + ": " + message);
+                clientHandles.get(i).printMessageWriter.flush();
             }
-        } catch (Exception ex) {
-            ex.printStackTrace();
+        } else {
+            clientHandles.get(index).printMessageWriter.println("Unrecognizable request");
+            clientHandles.get(index).printMessageWriter.flush();
+
         }
     }
 
     public void run() {
-        inputStreams = new ArrayList();
+        clientHandles = new ArrayList();   //client list
+
         try {
             ServerSocket serverSocket = new ServerSocket(4242);
             System.out.println("Server online. Server version (1.0.1).\n");
+
             while (true) {
-                Socket clientSocket = serverSocket.accept();
-                PrintWriter printWriter = new PrintWriter(clientSocket.getOutputStream());
-                inputStreams.add(printWriter);
-                threadPool.execute(new ClientHandle(clientSocket));
+                Socket clientSocket = serverSocket.accept(); //accept request
+
+                //receive connection message
+                InputStreamReader inputStreamReader = new InputStreamReader(clientSocket.getInputStream());
+                BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+
+
+                clientHandles.add(new ClientHandle(clientSocket, (clientHandles.size()))); //add client to database
+                threadPool.execute(clientHandles.get(clientHandles.size() - 1)); //execute client thread
+                clientHandles.get(clientHandles.size() - 1).name = bufferedReader.readLine(); //set client name
+
                 System.out.println("Connected new Client has connected.\t" + LocalDate.now() + ", " + LocalTime.now().withNano(0) + " " + clientSocket.getInetAddress());
+
+                amountConnected = clientHandles.size() - 1;
+                showConnected();
             }
         } catch (Exception ex) {
             ex.printStackTrace();
         }
     }
+
+    public void onDisconnection(int index) {
+        clientHandles.remove(index);
+        for (int i = 0; i < clientHandles.size(); i++) {
+            clientHandles.get(i).index = i;
+        }
+        amountConnected = clientHandles.size() - 1;
+    }
+
+    public void showConnected() {
+        System.out.println("Currently connected: ");
+        for (int i = 0; i < clientHandles.size(); i++) {
+            System.out.println(clientHandles.get(i).socketMessage.getInetAddress() + ", At index: " + clientHandles.get(i).index + ", As: " + clientHandles.get(i).name + ", Is connected: " + clientHandles.get(i).isConnected);
+        }
+    }
+
 
     public static void main(String[] argv) {
         Server client = new Server();
